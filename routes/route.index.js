c4694b241f0bdbfc13c9d23b0cd6ed39c1a733b1
Regis <regisrifaldi8@gmail.com>
2024-05-27 15:49:49 +0700

    feat: add Forgot Password Section

--- routes/route.index.js
+++ routes/route.index.js
@@ -6,15 +6,30 @@ const {
   login,
   firstPage,
   verifyEmail,
+  forgotPassword,
+  resetPassword,
 } = require("../controllers/auth.controllers");
 
 const restrict = require("../middleware/restrict");
 
-// router api auth
+// all router api auth
+
+// register
 router.post("/register", register);
+
+//login
 router.post("/login", login);
+
+// firstPage
 router.get("/", restrict, firstPage);
 
+// verifikasi email
 router.get("/verifikasi", verifyEmail);
+
+// forgot password
+router.post("/forgot-password", forgotPassword);
+
+// reset password
+router.post("/reset-password", resetPassword);
 
 module.exports = router;
