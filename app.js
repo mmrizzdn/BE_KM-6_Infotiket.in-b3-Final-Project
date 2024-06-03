e2d956a3beba4b6121b25b4959e01efbba0c7d7a
mmrizzdn <zoed.eldien15@gmail.com>
2024-06-03 20:36:40 +0700

    seeding database to vercel and deactivate swagger

--- app.js
+++ app.js
@@ -11,15 +11,15 @@ app.use(cors());
 app.use(cookieParser());
 
 // Swagger
-const swaggerUI = require("swagger-ui-express");
-const YAML = require("yaml");
+// const swaggerUI = require("swagger-ui-express");
+// const YAML = require("yaml");
 const fs = require("fs");
-const file = fs.readFileSync("./api-docs.yaml", "utf-8");
-const swaggerDocument = YAML.parse(file);
+// const file = fs.readFileSync("./api-docs.yaml", "utf-8");
+// const swaggerDocument = YAML.parse(file);
 
 // All Routers
 // Api Docs
-app.use("/api/v1/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+// app.use("/api/v1/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // Api Login and Register
 const router = require("./routes/route.index");
