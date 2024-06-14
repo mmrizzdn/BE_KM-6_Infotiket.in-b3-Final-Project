9e06bef542fa9de3beb06a6eac6fca7000d90146
M. Ammar Izzudin <zoed.eldien15@gmail.com>
2024-06-14 17:46:59 +0700

    allow headers in cors

--- app.js
+++ app.js
@@ -8,7 +8,16 @@ const path = require("path");
 const port = process.env.PORT || 3000;
 
 const app = express();
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: "Content-Type, Authorization",
+  })
+);
+
 app.use(cookieParser());
 app.use(morgan("dev"));
 app.use(express.json());
