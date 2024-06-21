a421d95fe43b5a32065b6a65d25c79b4f4186090
Karepto <renggadwipribadi@gmail.com>
2024-06-21 13:11:17 +0700

    update cron

--- libs/cron.js
+++ libs/cron.js
@@ -63,7 +63,7 @@ const generateFlightSchedules = async () => {
 generateFlightSchedules();
 
 // Atur tugas cron untuk menjalankan setiap hari pukul 00:00
-let task = cron.schedule("0 0 * * *", generateFlightSchedules, {
+let task = cron.schedule("*/1 * * * *", generateFlightSchedules, {
   timezone: "Asia/Jakarta",
 });
 task.start();
